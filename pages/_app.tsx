f567c7b70a69768e43347fbd8f9ee3e315421d7c
jo-im <joann_im@hotmail.com>
2022-09-08 14:35:47 -0700

    Setup Facebook login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script>
+      {`window.fbAsyncInit = function() {
+        FB.init({
+          appId            : ${process.env.NEXT_PUBLIC_FB_APP_ID},
+          autoLogAppEvents : true,
+          xfbml            : true,
+          version          : 'v14.0'
+        });
+      };`}
+    </Script>
+    <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload"></Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
