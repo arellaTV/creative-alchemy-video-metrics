bdb9e3ba80a2eb1ccaabffd803c8e1a86cee698e
arellaTV <jay@creativealchemy.org>
2022-09-10 10:43:32 -0700

    Checks session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
