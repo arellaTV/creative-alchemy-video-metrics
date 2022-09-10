36a598ef185c86a1374b741f1f1e523e38dc16aa
arellaTV <jay@creativealchemy.org>
2022-09-10 10:55:57 -0700

    Logs the status

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import styles from '../styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home: NextPage = () => {
-  const { data: session } = useSession()
-  console.log({ session })
+  const { data: session, status } = useSession()
+  console.log({ session, status })
 
   return (
     <div className={styles.container}>
